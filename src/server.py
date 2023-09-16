6e5a3f379d1a04ebc930b19d462414a50684c8ce
gabitenoriob <gabitenoriob@gmail.com>
2023-09-16 10:16:30 -0300

    server ok

--- src/server.py
+++ src/server.py
@@ -1,11 +1,36 @@
 import socket
 import json
+import random
 
+#ler perguntas
+with open('trivia.json', 'r') as file: 
+    perguntas = json.load(file)
+
+#criar socket server
 serverPort = 12345
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind(('', serverPort))
+serverSocket.listen(2) #numero máximo de conexoes = 2 players
+    
+print("Servidor pronto para receber conexões...")
 
-serverSocket.listen(2)
+
+#FUNÇÃO p/ enviar a pergunta para o cliete 
+
+def enviar_pergunta(clientSocket):
+    pergunta = random.choice(perguntas)
+    clientSocket.send(json.dumps(pergunta).encode()) #converte para bytes antes de enviar o dado logo te que desconverter quando chegar
+    
+    
+#conexão com o cliente
 
 
-#enviar a pergunta para o cliete 
+while True:
+    clientSocket, clientAdress = serverSocket.accept()
+    print(f"Conexão estabelecida com {clientAdress}")
+    
+    #enviar perguntas para o cliente
+    for _ in range(len(perguntas)):
+        enviar_pergunta(clientSocket)
+        
+    clientSocket.close()
