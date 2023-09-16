081e1d1c65b9cb6752e53ec599e60f9895d2d059
gabitenoriob <gabitenoriob@gmail.com>
2023-09-16 10:57:09 -0300

    versao para primeiros testes

--- src/client.py
+++ src/client.py
@@ -3,17 +3,75 @@ import threading
 import pygame
 import json
 
-
+# configurações do socket client 
 serverName = 'servername'
 serverPort = 12345
-
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
 # codigo py game 
 
+pygame.init()
+
+#tela
+WIDTH, HEIGHT =  800, 600
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Trivia")
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+FONT = pygame.font.Font("C:\Users\gabit\Downloads\04b_30\04B_30__.TTF",36)
+
+#FUNCAO exibir a pergunta 
+def display_pergunta():
+    texto_pergunta = FONT.render(pergunta_atual['pergunta'], True, BLACK)
+    SCREEN.blit(texto_pergunta(50,50))
+    
+#FUNCAO enviar RESPOSTA
+
+def enviar_resposta(resposta):
+    clientSocket.send(resposta.encode())
+    
+#nova pergunta
+
+def atualizar_pergunta():
+    pergunta = clientSocket.recv(1024).decode()
+    if not pergunta:
+        clientSocket.close()
+        pygame.quit()
+        quit()
+    else:
+        global pergunta_atual
+        pergunta_atual = json.loads(pergunta)
+        display_pergunta()
+        
+    pergunta_atual= None
+
+    
+    running = True
+    
+    while running:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        running = False
+                        
+                    elif event.type == pygame.KEYDOWN:
+                        
+                        if event.key == pygame.K_a:
+                         enviar_resposta('A')
+                         atualizar_pergunta()
+                        elif event.key == pygame.K_b:
+                            enviar_resposta('B')
+                            atualizar_pergunta()
+                        elif event.key == pygame.K_c:
+                            enviar_resposta('C')
+                            atualizar_pergunta()
+                        elif event.key == pygame.K_d:
+                            enviar_resposta('D')
+                            atualizar_pergunta()
+        
+                    pygame.display.flip()
+        
+    pygame.quit()
 
 
-#enviar resposta ao servidor e fechar jogo E cliente 
-clientSocket.close()
 
--- src/server.py
+++ src/server.py
@@ -25,12 +25,35 @@ def enviar_pergunta(clientSocket):
 #conexão com o cliente
 
 
+
 while True:
-    clientSocket, clientAdress = serverSocket.accept()
-    print(f"Conexão estabelecida com {clientAdress}")
+    jogador1_pontuacao = 0
+    jogador2_pontuacao = 0
+    jogador1_socket, jogador1_adress = serverSocket.accept()
+    print(f"Jogador 1 conectado: {jogador1_adress}")
+    jogador2_socket, jogador2_adress = serverSocket.accept()
+    print(f"Jogador 2 conectado: {jogador2_adress}")
+
+    for _ in range(len(perguntas)): 
+
+        enviar_pergunta(jogador1_socket)
+        enviar_pergunta(jogador2_socket)
+
+        # pegar respostas
+        resposta_jogador1 = jogador1_socket.recv(1024).decode()
+        resposta_jogador2 = jogador2_socket.recv(1024).decode()
+
+        # verificar e somar pontos
+        pergunta_atual = perguntas[_]
+        if resposta_jogador1.lower() == pergunta_atual['resposta'].lower():
+            jogador1_pontuacao += 1
+        if resposta_jogador2.lower() == pergunta_atual['resposta'].lower():
+            jogador2_pontuacao += 1
+
+    # pontuações finais 
+    jogador1_socket.send(f"Sua pontuação final: {jogador1_pontuacao}".encode())
+    jogador2_socket.send(f"Sua pontuação final: {jogador2_pontuacao}".encode())
+
     
-    #enviar perguntas para o cliente
-    for _ in range(len(perguntas)):
-        enviar_pergunta(clientSocket)
-        
-    clientSocket.close()
+    jogador1_socket.close()
+    jogador2_socket.close()
